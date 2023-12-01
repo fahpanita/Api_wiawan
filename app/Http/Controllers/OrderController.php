1fafa4f8a99553c614183d9514fa246d0f290bea
fahh <fahpanita86@gmail.com>
2023-12-01 21:59:15 +0700

    order//

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -56,7 +56,7 @@ class OrderController extends Controller
         $order->user_id = Auth::user()->id;
         $order->status = "Check Payment";
         $order->type_shipping = "รับหน้าร้าน";
-        $order->type_shipping = $data->type_shipping;
+        // $order->type_shipping = $data->type_shipping;
         $order->order_date = $datetime->format('D-M-Y');
 
 
