b09ab8168c04f6795152a2882082693285a818f5
fahh <fahpanita86@gmail.com>
2024-02-17 02:58:01 +0700

    up-prepro

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -27,20 +27,35 @@ class PaymentsController extends Controller
 
         $data = json_decode($request->getContent());
         $array_data = (array) $data;
+
         $validator = Validator::make($array_data, [
             'order_id' => 'required|exists:orders,id',
-            'price' => 'required'
         ]);
 
         if ($validator->fails()) {
             return response()->json(["message" => $validator->errors()->first()], 400);
         }
 
-        $payments = Payment::get();
+        $order_id = $request->input('order_id');
+
+        $totalPriceSum = (float)OrderItem::where('order_id', $order_id)
+            ->sum(DB::raw('price * amount'));
+
+        // dd($totalPriceSum);
+
+        $priceShiping = (float)DB::table('order_items')
+            ->select('products.typeShipping')
+            ->join('products', 'order_items.product_id', '=', 'products.id')
+            ->where('order_items.order_id', '=', $request->order_id)
+            ->orderBy('products.typeShipping', 'desc')
+            ->first()->typeShipping;
+
+
+        $totalPrice = $totalPriceSum + $priceShiping;
 
         $pp = new PromptPay();
         $target = '088-656-5433';
-        $promptPay = $data->price;
+        $promptPay = $totalPrice;
 
         $payload = $pp->generatePayload($target, $promptPay);
 
@@ -48,7 +63,17 @@ class PaymentsController extends Controller
 
         return response()->json(['payload' => $payload, 'amount' => $promptPay]);
     }
+    // $payments = Payment::get();
+    // $data = json_decode($request->getContent());
+    // $array_data = (array) $data;
+    // $validator = Validator::make($array_data, [
+    //     'order_id' => 'required|exists:orders,id',
+    //     'price' => 'required'
+    // ]);
 
+    // if ($validator->fails()) {
+    //     return response()->json(["message" => $validator->errors()->first()], 400);
+    // }
 
     public function create()
     {
